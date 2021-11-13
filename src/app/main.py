c23d2c69344cc502a4589429c2201fff8621795f
Leandro da Cruz Farias <leandro.da.cruz.farias@gmail.com>
2021-11-12 23:22:14 -0400

    Revert "Add model file and load it."
    This reverts commit 8e69567cf1b25b8a99108d7b42fa9599485dc49b.

--- src/app/main.py
+++ src/app/main.py
@@ -18,87 +18,36 @@ app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
 basic_auth = BasicAuth(app)
 
 # Antes das APIs
-columns = ['age', 'bmi', 'elective_surgery', 'ethnicity', 'gender', 'height',
-       'hospital_admit_source', 'icu_admit_source', 'icu_id',
-       'icu_stay_type', 'icu_type', 'pre_icu_los_days',
-       'readmission_status', 'weight', 'albumin_apache',
-       'apache_2_diagnosis', 'apache_3j_diagnosis',
-       'apache_post_operative', 'arf_apache', 'bilirubin_apache',
-       'bun_apache', 'creatinine_apache', 'fio2_apache',
-       'gcs_eyes_apache', 'gcs_motor_apache', 'gcs_unable_apache',
-       'gcs_verbal_apache', 'glucose_apache', 'heart_rate_apache',
-       'hematocrit_apache', 'intubated_apache', 'map_apache',
-       'paco2_apache', 'paco2_for_ph_apache', 'pao2_apache', 'ph_apache',
-       'resprate_apache', 'sodium_apache', 'temp_apache',
-       'urineoutput_apache', 'ventilated_apache', 'wbc_apache',
-       'd1_diasbp_invasive_max', 'd1_diasbp_invasive_min',
-       'd1_diasbp_max', 'd1_diasbp_min', 'd1_diasbp_noninvasive_max',
-       'd1_diasbp_noninvasive_min', 'd1_heartrate_max',
-       'd1_heartrate_min', 'd1_mbp_invasive_max', 'd1_mbp_invasive_min',
-       'd1_mbp_max', 'd1_mbp_min', 'd1_mbp_noninvasive_max',
-       'd1_mbp_noninvasive_min', 'd1_resprate_max', 'd1_resprate_min',
-       'd1_spo2_max', 'd1_spo2_min', 'd1_sysbp_invasive_max',
-       'd1_sysbp_invasive_min', 'd1_sysbp_max', 'd1_sysbp_min',
-       'd1_sysbp_noninvasive_max', 'd1_sysbp_noninvasive_min',
-       'd1_temp_max', 'd1_temp_min', 'h1_diasbp_invasive_max',
-       'h1_diasbp_invasive_min', 'h1_diasbp_max', 'h1_diasbp_min',
-       'h1_diasbp_noninvasive_max', 'h1_diasbp_noninvasive_min',
-       'h1_heartrate_max', 'h1_heartrate_min', 'h1_mbp_invasive_max',
-       'h1_mbp_invasive_min', 'h1_mbp_max', 'h1_mbp_min',
-       'h1_mbp_noninvasive_max', 'h1_mbp_noninvasive_min',
-       'h1_resprate_max', 'h1_resprate_min', 'h1_spo2_max', 'h1_spo2_min',
-       'h1_sysbp_invasive_max', 'h1_sysbp_invasive_min', 'h1_sysbp_max',
-       'h1_sysbp_min', 'h1_sysbp_noninvasive_max',
-       'h1_sysbp_noninvasive_min', 'h1_temp_max', 'h1_temp_min',
-       'd1_albumin_max', 'd1_albumin_min', 'd1_bilirubin_max',
-       'd1_bilirubin_min', 'd1_bun_max', 'd1_bun_min', 'd1_calcium_max',
-       'd1_calcium_min', 'd1_creatinine_max', 'd1_creatinine_min',
-       'd1_glucose_max', 'd1_glucose_min', 'd1_hco3_max', 'd1_hco3_min',
-       'd1_hemaglobin_max', 'd1_hemaglobin_min', 'd1_hematocrit_max',
-       'd1_hematocrit_min', 'd1_inr_max', 'd1_inr_min', 'd1_lactate_max',
-       'd1_lactate_min', 'd1_platelets_max', 'd1_platelets_min',
-       'd1_potassium_max', 'd1_potassium_min', 'd1_sodium_max',
-       'd1_sodium_min', 'd1_wbc_max', 'd1_wbc_min', 'h1_albumin_max',
-       'h1_albumin_min', 'h1_bilirubin_max', 'h1_bilirubin_min',
-       'h1_bun_max', 'h1_bun_min', 'h1_calcium_max', 'h1_calcium_min',
-       'h1_creatinine_max', 'h1_creatinine_min', 'h1_glucose_max',
-       'h1_glucose_min', 'h1_hco3_max', 'h1_hco3_min',
-       'h1_hemaglobin_max', 'h1_hemaglobin_min', 'h1_hematocrit_max',
-       'h1_hematocrit_min', 'h1_inr_max', 'h1_inr_min', 'h1_lactate_max',
-       'h1_lactate_min', 'h1_platelets_max', 'h1_platelets_min',
-       'h1_potassium_max', 'h1_potassium_min', 'h1_sodium_max',
-       'h1_sodium_min', 'h1_wbc_max', 'h1_wbc_min',
-       'd1_arterial_pco2_max', 'd1_arterial_pco2_min',
-       'd1_arterial_ph_max', 'd1_arterial_ph_min', 'd1_arterial_po2_max',
-       'd1_arterial_po2_min', 'd1_pao2fio2ratio_max',
-       'd1_pao2fio2ratio_min', 'h1_arterial_pco2_max',
-       'h1_arterial_pco2_min', 'h1_arterial_ph_max', 'h1_arterial_ph_min',
-       'h1_arterial_po2_max', 'h1_arterial_po2_min',
-       'h1_pao2fio2ratio_max', 'h1_pao2fio2ratio_min', 'aids',
-       'cirrhosis', 'hepatic_failure', 'immunosuppression', 'leukemia',
-       'lymphoma', 'solid_tumor_with_metastasis']
+colunas = ['RevolvingUtilizationOfUnsecuredLines', 'age',
+       'NumberOfTime30-59DaysPastDueNotWorse', 'DebtRatio', 'MonthlyIncome',
+       'NumberOfOpenCreditLinesAndLoans', 'NumberOfTimes90DaysLate',
+       'NumberRealEstateLoansOrLines', 'NumberOfTime60-89DaysPastDueNotWorse',
+       'NumberOfDependents', 'IncomePerPerson', 'NumOfPastDue', 'MonthlyDebt',
+       'NumOfOpenCreditLines', 'MonthlyBalance', 'age_sqr']
 
-# Load trained model
-model = pickle.load(open('models/xgboost_model.pkl', "rb"))
+def load_model(file_name = 'xgboost_undersampling.pkl'):
+    return pickle.load(open(file_name, "rb"))
 
-# Route to predict health status
-@app.route('/status/', methods=['POST'])
+# Carregar modelo treinado
+modelo = load_model('models/xgboost_undersampling.pkl')
+
+# Rota de predição de scores
+@app.route('/score/', methods=['POST'])
 @basic_auth.required
-def get_health_status():
-    # Get JSON from request
-    data = request.get_json()
-
-    # Load data
-    payload = np.array([[data[column] for column in columns]])
-
-    # Predict
-    prediction = int(model.predict(payload)[0])
-    status = 'Saudável'
-
-    if prediction == 1:
-        status = 'Diabético'
-
-    return jsonify(status=status)
+def get_score():
+    # Pegar o JSON da requisição
+    dados = request.get_json()
+    # Garantir a ordem das colunas
+    payload = np.array([dados[col] for col in colunas])
+    # Fazer predição
+    payload = xgb.DMatrix([payload], feature_names=colunas)
+    score = np.float64(modelo.predict(payload)[0])
+    status = 'APROVADO'
+    if score <= 0.3:
+        status = 'REPROVADO'
+    elif score <= 0.6: 
+        status = 'MESA DE AVALIACAO'
+    return jsonify(cpf=dados['cpf'], score=score, status=status)
 
 # Nova rota - recebendo CPF
 @app.route('/healthcare/<cpf>')
@@ -109,7 +58,7 @@ def show_cpf(cpf):
 # Rota padrão
 @app.route('/')
 def home():
-    return 'API de Diagnóstico de Diabetes'
+    return 'API de Diagnostico de Diabetes'
 
 # Subir a API
 app.run(debug=True, host='0.0.0.0')
